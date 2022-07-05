fe9028cc203ed27898b0f2cde2b07fff1130a364
Neil Ryan Lipa-od <neilryanlipaod@gmail.com>
2022-07-05 16:51:28 +0800

    add /{name} endpoint

--- docker-practice/src/main/java/com/indifferentcat/dockerpractice/controller/HelloPhilip.java
+++ docker-practice/src/main/java/com/indifferentcat/dockerpractice/controller/HelloPhilip.java
@@ -1,6 +1,7 @@
 package com.indifferentcat.dockerpractice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class HelloPhilip {
     @GetMapping
     public String helloPhilip() {
         return "Hello, my name is Philip!";
+    }
+
+    @GetMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 }
