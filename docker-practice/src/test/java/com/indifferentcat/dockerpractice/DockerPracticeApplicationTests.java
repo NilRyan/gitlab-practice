f40fe2de6e4724b4ea33d4476b4cbe3cb99204f8
Neil Ryan Lipa-od <neilryanlipaod@gmail.com>
2022-07-05 14:23:33 +0800

    Initialize repository

---
+++ docker-practice/src/test/java/com/indifferentcat/dockerpractice/DockerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indifferentcat.dockerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
